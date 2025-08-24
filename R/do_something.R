32f9765ba30fb248d3276fc0fe83be76cead1496
Westphal <max.westphal@mevis.fraunhofer.de>
2025-08-24 15:00:58 +0200

    added the new feature 123

---
+++ R/do_something.R
@@ -0,0 +1,12 @@
+#' Do something
+#'
+#' @param n an integer, how much do you want?
+#'
+#' @returns your something (a vector of standard normal numbers)
+#' @export
+#'
+#' @examples
+#' do_something(42)
+do_something <- function(n=5){
+  rnorm(n)
+}
